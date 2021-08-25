68b1dde20acc9a7fc27a0a1c70486ff5d2fa94c0
adonis-mac <adonisdoda@gmail.com>
2021-08-25 18:12:38 -0300

    salvar cupom na carteira

--- src/components/component_heavy/modal_save_coupon/index.tsx
+++ src/components/component_heavy/modal_save_coupon/index.tsx
@@ -3,21 +3,33 @@ import { View } from 'react-native';
 import { ModalComponent } from '../../component/modal';
 import { Cursor, RenderCellOptions, useClearByFocusCell } from 'react-native-confirmation-code-field';
 
-import { Input, Wrapper } from './styles';
-import { ButtonCreateCoupon } from '../../component/button_cupon';
+import { Input, Container, Wrapper } from './styles';
 import { ButtonBox } from '../../component/button_box';
+import { CouponService } from '../../../services/coupon_service';
 
 export const ModalSaveCoupon: React.FC<{ visible: boolean, onClose: any }> = (props) => {
 
   const [value, setValue] = useState("")
 
 
+  const onSaveCoupon = async () => {
+    try {
+
+      await CouponService.saveCouponInWallet(value)
+
+    } catch (error) {
+
+    }
+  }
+
   return (
 
     <ModalComponent visible={props.visible} onClose={() => props.onClose()}>
       <Wrapper>
-        <Input value={value} onChangeText={(e) => setValue(e)} />
-        <ButtonBox onPress={() => { }} size={25} />
+        <Container>
+          <Input value={value} onChangeText={(e) => setValue(e)} />
+          <ButtonBox onPress={() => { }} size={25} />
+        </Container>
       </Wrapper>
     </ModalComponent >
   );
