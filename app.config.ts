58799509e8f9c84456c3aff1a3c3e991d49a454c
adonis <adonisdoda@gmail.com>
2021-11-02 03:38:59 -0300

    ajustes app config

--- app.config.ts
+++ app.config.ts
@@ -33,7 +33,11 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   android: {
     package: "app.klubbs.user",
     permissions: ['CAMERA', 'ACCESS_FINE_LOCATION', 'ACCESS_BACKGROUND_LOCATION', 'ACCESS_COARSE_LOCATION'],
-    versionCode: 4
+    versionCode: 4,
+    adaptiveIcon: {
+      foregroundImage: './assets/images/icon.png',
+      backgroundColor: "#1F1F1F"
+    }
   },
   extra: {
     ENVIRONMENT_API_URL: process.env.ENVIRONMENT_API_URL
