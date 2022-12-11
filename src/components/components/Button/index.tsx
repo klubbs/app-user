7674a0bfb82a83cfb86ae29e0496764a802f5a01
adonisantunesuessler <201904085822@alunos.estacio.br>
2022-12-10 23:13:26 -0300

    corrigindo bug de horário de produçaão

--- src/components/components/Button/index.tsx
+++ src/components/components/Button/index.tsx
@@ -3,35 +3,39 @@ import { Title, Wrapper } from './styles';
 import { Feather } from '@expo/vector-icons';
 import { colors } from '../../../../assets/constants/colors';
 
-
 export type IButtonProps = {
-  text: string
-  onPress: () => void
-  styleContainer?: any
-  textColor?: any
-  featherIcon?: any
-  disabled?: boolean
-}
+  text: string;
+  onPress: () => void;
+  styleContainer?: any;
+  textColor?: any;
+  featherIcon?: any;
+  disabled?: boolean;
+  asdkjh;
+};
 
-const Button: React.FC<IButtonProps> = ({ text, onPress, styleContainer, textColor, featherIcon, disabled }) => {
+const Button: React.FC<IButtonProps> = ({
+  text,
+  onPress,
+  styleContainer,
+  textColor,
+  featherIcon,
+  disabled,
+}) => {
   return (
-    <Wrapper
-      disabled={disabled}
-      onPress={onPress}
-      style={{ ...styleContainer }}
-    >
-      <Title disabled={disabled} color={textColor}>{text}</Title>
-      {
-        featherIcon &&
+    <Wrapper disabled={disabled} onPress={onPress} style={{ ...styleContainer }}>
+      <Title disabled={disabled} color={textColor}>
+        {text}
+      </Title>
+      {featherIcon && (
         <Feather
           name={featherIcon}
           size={16}
           color={textColor ?? colors.COLOR_WHITE}
           style={{ left: 10 }}
         />
-      }
+      )}
     </Wrapper>
   );
-}
+};
 
 export default Button;
