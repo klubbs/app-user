8e5b7554dc25116e51070ae5eb231d624bd9a67b
adonis <adonisdoda@gmail.com>
2022-12-14 01:53:46 -0300

    Finalizando fluxo v1

--- src/components/screens/user-profile/styles.ts
+++ src/components/screens/user-profile/styles.ts
@@ -2,18 +2,16 @@ import { Feather } from '@expo/vector-icons';
 import styled from 'styled-components/native';
 import { colors } from '../../../../assets/constants/colors';
 
-
 export const SafeArea = styled.SafeAreaView`
-  flex:1;
+  flex: 1;
   background-color: ${colors.COLOR_WHITE};
+`;
 
-`
-
-export const IconLogout = styled(Feather).attrs(props => ({
+export const IconLogout = styled(Feather).attrs((props) => ({
   name: 'log-out',
   size: 16,
-  colors: colors.COLOR_BLACK40
-}))``
+  colors: colors.COLOR_BLACK40,
+}))``;
 
 export const ContainerPoints = styled.View`
   border-left-width: 0.8px;
@@ -26,7 +24,7 @@ export const ContainerPoints = styled.View`
 `;
 
 export const UserImage = styled.Image.attrs({
-  resizeMode: 'contain'
+  resizeMode: 'contain',
 })`
   width: 70px;
   height: 70px;
@@ -47,27 +45,25 @@ export const WrapperTop = styled.View`
 `;
 
 export const PointValues = styled.Text`
-  color:${colors.COLOR_BLACK40};
-  font-size:14px;
-  font-family:'Nunito_Light';
-`
-
+  color: ${colors.COLOR_BLACK40};
+  font-size: 14px;
+  font-family: 'Nunito_Light';
+`;
 
 export const Point = styled.Text`
-  color:${colors.COLOR_BLACK80};
-  font-size:14px;
-  font-family:'Nunito_Regular';
-`
+  color: ${colors.COLOR_BLACK80};
+  font-size: 14px;
+  font-family: 'Nunito_Regular';
+`;
 
 export const MenuTextContainer = styled.View`
   flex: 1;
   align-items: flex-start;
   padding-left: 5%;
-`
+`;
 
-
-export const MenuLogoutContainer = styled.TouchableOpacity.attrs(props => ({
-  activeOpacity: 0.8
+export const MenuLogoutContainer = styled.TouchableOpacity.attrs((props) => ({
+  activeOpacity: 0.8,
 }))`
   margin-top: 50px;
   padding-left: 2%;
@@ -78,12 +74,11 @@ export const MenuLogoutContainer = styled.TouchableOpacity.attrs(props => ({
 `;
 
 export const MenuTextLogout = styled.Text`
-  color:${colors.COLOR_BLACK40};
-  font-size:16px;
-  font-family:'Nunito_Regular';
-`
+  color: ${colors.COLOR_BLACK40};
+  font-size: 16px;
+  font-family: 'Nunito_Regular';
+`;
 
 export const ContainerScroll = styled.ScrollView`
-	padding:20px;
-`
-
+  padding: 20px;
+`;
