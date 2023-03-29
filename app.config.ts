75187ad151914f26759fd6e961c5da30f69a18e3
adonis <adonisdoda@gmail.com>
2023-03-29 02:43:34 -0300

    increase build

--- app.config.ts
+++ app.config.ts
@@ -20,7 +20,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ios: {
     supportsTablet: false,
     bundleIdentifier: 'app.klubbs.user',
-    buildNumber: '0.0.19',
+    buildNumber: '0.0.20',
     icon: './assets/images/ios-icon.png',
     infoPlist: {
       NSLocationAlwaysAndWhenInUseUsageDescription:
@@ -32,7 +32,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   android: {
     package: 'app.klubbs.user',
     permissions: ['CAMERA', 'ACCESS_FINE_LOCATION', 'ACCESS_COARSE_LOCATION'],
-    versionCode: 18,
+    versionCode: 19,
     adaptiveIcon: {
       foregroundImage: './assets/images/android-icon.png',
       backgroundColor: '#1F1F1F',
